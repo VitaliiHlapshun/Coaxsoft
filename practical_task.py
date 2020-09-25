b7a6dead12454c0b2fd33d61fd7bb7f30bf06261
Vitalii <65821209+VitaliiHlapshun@users.noreply.github.com>
2020-09-25 07:47:27 +0300

    Update practical_task.py

--- practical_task.py
+++ practical_task.py
@@ -6,47 +6,47 @@ class BookNote:
     notes_file = 'notes.json'
 
     def __init__(self):
-self.notes_data = dict()
+        self.notes_data = dict()
 
-def add_note(self, note, author_name, author_rate=0.0):
-if not author_rate:
+    def add_note(self, note, author_name, author_rate=0.0):
+        if not author_rate:
             author_rate = round(random.random(), 1)
-self.notes_data[note] = {'author': author_name, 'author_rate': author_rate}
-print(f'Note {note} of {author_name} author {self.notes_file} added.')
-self.write_file_with_content(self.notes_data)
+            self.notes_data[note] = {'author': author_name, 'author_rate': author_rate}
+            print(f'Note {note} of {author_name} author {self.notes_file} added.')
+            self.write_file_with_content(self.notes_data)
 
-def write_file_with_content(self, content: dict):
-with open(self.notes_file, 'w') as f:
+    def write_file_with_content(self, content: dict):
+        with open(self.notes_file, 'w') as f:
             json.dump(content, f, sort_keys=True, indent=4)
-print(f'Notes file {self.notes_file} has been updated with content of {content}.')
+            print(f'Notes file {self.notes_file} has been updated with content of {content}.')
 
-def get_notes_from_file(self):
-with open(self.notes_file) as f:
+    def get_notes_from_file(self):
+        with open(self.notes_file) as f:
             notes = json.load(f)
-print('Current notes read from notes.json file >> ', notes)
-return notes
+            print('Current notes read from notes.json file >> ', notes)
+        return notes
 
-def get_author_with_highest_rate(self):
+    def get_author_with_highest_rate(self):
         highest_rate = max(data['author_rate'] for data in self.notes_data.values())
         best_rated_authors = [data['author'] for data in self.notes_data.values() if data['author_rate'] == highest_rate]
-# best_rated_authors = []
+        # best_rated_authors = []
         # for a, data in self.notes_data.items():
         #     if data['b'] == max:
         #         best_rated_authors.append(a)
         print(f'Authors list with highest rate of {highest_rate}: {best_rated_authors}')
-return best_rated_authors
+        return best_rated_authors
 
-def get_author_with_lowest_rate(self):
+    def get_author_with_lowest_rate(self):
         lowest_rate = min(data['author_rate'] for data in self.notes_data.values())
         worst_rated_authors = [data['author'] for data in self.notes_data.values() if data['author_rate'] == lowest_rate]
-print(f'Authors list with lowest rate of {lowest_rate}: {worst_rated_authors}')
-return worst_rated_authors
+        print(f'Authors list with lowest rate of {lowest_rate}: {worst_rated_authors}')
+        return worst_rated_authors
 
-def get_authors_average_rate(self):
+    def get_authors_average_rate(self):
         average_rate = sum(data['author_rate'] for data in self.notes_data.values()) / len(self.notes_data.values())
         average_rate = round(average_rate, 1)
-print(f'Average rate of all authors: {average_rate}')
-return average_rate
+        print(f'Average rate of all authors: {average_rate}')
+        return average_rate
 
 
 if __name__ == '__main__':
