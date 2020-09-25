5f9e374ad7d2ee17aeda367e59a0c691a5fa3af7
Vitalii <65821209+VitaliiHlapshun@users.noreply.github.com>
2020-09-25 07:32:45 +0300

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -1,22 +1,19 @@
 import time
 
-
 def measure_func_execution_time(func):
-def wrapper():
+    def wrapper():
         st = time.perf_counter()
         res = func()
         execution_time = time.perf_counter() - st
-print('execution_time >> ', execution_time)
-return res
-return wrapper
-
+        print('execution_time >> ', execution_time)
+        return res
+    return wrapper
 
 @measure_func_execution_time
 def generate_huge_list():
     res = []
-for i in range(100000):
+    for i in range(100000):
         res.append(i)
-return res
-
+    return res
 
 generate_huge_list()
