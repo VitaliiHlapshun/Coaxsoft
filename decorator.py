b3d53c29022a6a0fdc09eb873cf0a0edaefe6f8b
Vitalii <65821209+VitaliiHlapshun@users.noreply.github.com>
2020-09-25 06:17:13 +0300

    Create decorator.py

---
+++ decorator.py
@@ -0,0 +1,22 @@
+import time
+
+
+def measure_func_execution_time(func):
+def wrapper():
+        st = time.perf_counter()
+        res = func()
+        execution_time = time.perf_counter() - st
+print('execution_time >> ', execution_time)
+return res
+return wrapper
+
+
+@measure_func_execution_time
+def generate_huge_list():
+    res = []
+for i in range(100000):
+        res.append(i)
+return res
+
+
+generate_huge_list()
